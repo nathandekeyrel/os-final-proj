4fd8a560cbbcc0872a6320cdf76cee7136ac1f7a
nathandekeyrel <natedekeyrel@gmail.com>
2024-11-30 09:47:04 -0700

    Remove CMake build files from git tracking and add proper gitignore

--- cmake-build-debug/CMakeFiles/edit_cache.dir/utility.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-cd /cygdrive/c/Users/nated/os-final-proj/cmake-build-debug
-/cygdrive/c/Users/nated/AppData/Local/JetBrains/CLion2023.3/cygwin_cmake/bin/cmake.exe -E echo No\ interactive\ CMake\ dialog\ available.
