41faebc9820ecb7e588b3bc26fcd678d46c392e8
nathandekeyrel <natedekeyrel@gmail.com>
2024-11-29 23:26:16 -0700

    restructure for current scope

--- src/core/process.h
+++ src/core/process.h
@@ -8,7 +8,6 @@
 //======================================================
 // State Enum
 //======================================================
-
 typedef enum process_state {
     PROCESS_NEW,
     PROCESS_READY,
@@ -20,7 +19,6 @@ typedef enum process_state {
 //======================================================
 // Process Structure
 //======================================================
-
 typedef struct process {
 
     // basic for all processes
@@ -48,28 +46,24 @@ typedef struct process {
 //======================================================
 // Process Stuff
 //======================================================
-
 process_t* process_create(int pid, int priority, unsigned long burst_time);
 void destroy_process(process_t* process);
 
 //======================================================
 // State Management
 //======================================================
-
 void process_set_state(process_t* process, process_state_t new_state);
 bool process_is_finished(const process_t* process);
 
 //======================================================
 // Runtime Management
 //======================================================
-
 void process_update_runtime(process_t* process, process_state_t new_state);
 void process_handle_io(process_t* process);
 
 //======================================================
 // Metric Helpers
 //======================================================
-
 double process_get_response_time(const process_t* process);
 double process_get_turnaround_time(const process_t* process);
 double process_get_waiting_time(const process_t* process);
--- src/core/queue.h
+++ src/core/queue.h
@@ -14,14 +14,12 @@ typedef struct queue {
 //======================================================
 // Queue Create/Destroy
 //======================================================
-
 queue_t* queue_create(void);
 void queue_destroy(queue_t* queue);
 
 //======================================================
 // Basic Queue Operations
 //======================================================
-
 bool queue_is_empty(const queue_t* queue);
 int queue_size(const queue_t* queue);
 void queue_clear(queue_t* queue);
@@ -29,7 +27,6 @@ void queue_clear(queue_t* queue);
 //======================================================
 // Process Management
 //======================================================
-
 bool queue_push(queue_t* queue, process_t* process);
 process_t* queue_pop(queue_t* queue);
 process_t* queue_peek(const queue_t* queue);
@@ -38,7 +35,6 @@ process_t* queue_peek(const queue_t* queue);
 //======================================================
 // Queue Traversal
 //======================================================
-
 typedef void (*queue_process_fn)(process_t* process, void* data);
 void queue_foreach(queue_t* queue, queue_process_fn fn, void* data);
 
