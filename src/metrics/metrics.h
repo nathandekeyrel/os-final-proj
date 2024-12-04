3739adf78e11f866db3e86f09ae3b9a38b0b69f4
nathandekeyrel <natedekeyrel@gmail.com>
2024-12-03 19:01:32 -0700

    lost some stuff in the merge

---
+++ src/metrics/metrics.h
@@ -0,0 +1,16 @@
+#ifndef METRICS_H
+#define METRICS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "../core/queue.h"
+#include "../core/process.h"
+
+double calculate_vruntime_variance(queue_t *queue);
+
+double process_get_response_time(const process_t *process);
+
+double process_get_turnaround_time(const process_t *process);
+
+#endif
